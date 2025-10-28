b4ec583b28a0b54aff5cc24744f46f9e2d49a5c8
Dhanush N <113281686+Dhanush-Nagarajan@users.noreply.github.com>
2025-10-28 16:55:24 +0530

    Revert "Dhanush stage"

--- src/modals/base/base.schema.js
+++
@@ -1,7 +0,0 @@
-const baseSchema = {
-  createdAt: { type: Date, required: true, default: () => new Date() },
-  updatedAt: { type: Date, default: () => new Date() },
-  status: { type: Boolean, default: true },
-};
-
-export { baseSchema };
